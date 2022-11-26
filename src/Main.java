51202502a6912fdbbc49ce498345ed0f60055e17
KosenkoAleksandra <kosenkoyes@gmail.com>
2022-11-26 19:18:42 +0300

    Часть 2. Циклы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //Часть 1
         // Домашнее задание 1
         //Задание 1
         System.out.println("Задание 1");
@@ -63,5 +64,110 @@ public class Main {
             sumTotal = sumTotal + deposit;
             System.out.println("Месяц " + i + " сумма накоплений равна " + sumTotal + " рублей");
         }
+
+        //Часть 2
+        // Домашнее задание 1
+        //Задание 1
+        System.out.println("Часть 2. Домашнее задание 1. Задание 1");
+        deposit = 15000;
+        sumTotal = 0;
+        int i = 0;
+        while (sumTotal <= 2_459_000) {
+            sumTotal = sumTotal + sumTotal/100;
+            sumTotal = sumTotal + deposit;
+            i += 1;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + sumTotal + " рублей");
+        }
+        //Задание 2
+        System.out.println("Домашнее задание 1. Задание 2");
+        i = 0;
+        while (i <10) {
+            i++;
+            System.out.println(i);
+        }
+        for ( i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        //Задание 3
+        System.out.println("Домашнее задание 1. Задание 3");
+        int populationOfTheCountryY = 12_000_000;
+        int born = 17;
+        int death = 8;
+        int diff = born - death;
+        for (i = 1; i<=10; i++){
+            populationOfTheCountryY += populationOfTheCountryY * diff/1000;
+            System.out.println("Год " + i + " численность населения составляет " +populationOfTheCountryY);
+        }
+//Часть 2.
+// Домашнее задание 2.
+        deposit = 15000;
+        sumTotal = 0;
+        int percent = 7; // в месяц
+// Задание 1
+        System.out.println("Часть 2. Домашнее задание 2. Задание 1");
+        i = 0;
+        while (sumTotal < 12_000_000) {
+            sumTotal = sumTotal + sumTotal*percent/100;
+            sumTotal = sumTotal + deposit;
+            i += 1;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + sumTotal + " рублей");
+
+        }
+        // Задание 2
+        System.out.println("Домашнее задание 2. Задание 2");
+        deposit = 15000;
+        sumTotal = 0;
+        for (i = 1; sumTotal < 12_000_000; i ++) {
+            sumTotal = sumTotal + sumTotal * percent / 100;
+            sumTotal = sumTotal + deposit;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + " сумма накоплений равна " + sumTotal + " рублей");
+            }
+        }
+        // Задание 3
+        System.out.println("Домашнее задание 2. Задание 3");
+        deposit = 15000;
+        sumTotal = 0;
+        percent = 7; // в месяц
+        int time = 9 * 12;
+        for (i = 1; i <= time; i ++) {
+            sumTotal = sumTotal + sumTotal * percent / 100;
+            sumTotal = sumTotal + deposit;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + " сумма накоплений равна " + sumTotal + " рублей");
+            }
+        }
+        // Задание 4
+        System.out.println("Домашнее задание 2. Задание 4");
+        int friday = 5;
+        int month = 31;
+        for (; friday <= month; friday += 7){
+            System.out.println("Сегодня пятница, " + friday + " число. Необходимо подготовить отчет.");
+        }
+        // Домашнее задание 3
+        //Задание 1
+        System.out.println("Часть 2. Домашнее задание 3. Задание 1");
+        int year = 0;
+        int nowYear = 2022;
+        int startYear = nowYear - 200;
+        int finishYear = nowYear + 100;
+        int period = 79;
+        while (year <= finishYear) {
+            if (year >= startYear) {
+                System.out.println(year);
+            }
+            year += period;
+        }
+        //Задание
+        System.out.println("Домашнее задание 3. Задание 2");
+        int number = 2;
+        for (i = 1; i <=10; i ++){
+            int result = number * i;
+            System.out.println(+ number + " * " + i + " = " + result);
+        }
+
+
+
+
     }
 }
